e6b6e9003d3ccb0733e9d07bbeb03deac3afedde
Vurnex <hamilton.laquon@gmail.com>
2023-03-11 14:52:58 -0600

    Test Netlify Function

--- js/script.js
+++ js/script.js
@@ -3,14 +3,46 @@ const handleSubmit = (event) => {
   
     const myForm = event.target;
     const formData = new FormData(myForm);
+
+    Promise.all([
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+          }),
+        fetch("/.netlify/functions/DBConfig")
+        .then(function() { 
+
+            formAlertFail.style.display = "none";
+            formAlertSuccess.style.display = "block"; })
+    
+          .catch(function(error) { 
+            
+            console.log(error); 
+            
+            formAlertSuccess.style.display = "none";
+            formAlertFail.style.display = "block"; })
+    ]);
+
+    /*
     
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(function() { formAlertSuccess.style.display = "block"; })
-      .catch(function(error) { formAlertFail.style.display = "block"; });
+      .then(function() { 
+
+        formAlertFail.style.display = "none";
+        formAlertSuccess.style.display = "block"; })
+
+      .catch(function(error) { 
+        
+        console.log(error); 
+        
+        formAlertSuccess.style.display = "none";
+        formAlertFail.style.display = "block"; }); 
+        */
   };
   
   document.querySelector("form").addEventListener("submit", handleSubmit);
